21d33f2086d9a7b319565212afe0164f89a4709b
João Matheus <joaomatheusantunes.dev@gmail.com>
2023-06-06 15:32:11 -0300

    alter build

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.listen(process.env.APP_PORT, () => console.log('server running'));
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.get('/', (req: Request, res: Response) => {
+    res.render('index')
+})
+
+app.listen(process.env.APP_PORT, () => console.log('server running'))
