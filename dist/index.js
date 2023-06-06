a42d5c9e83567682e34da619dc1ffa8c153e63b0
João Matheus <joaomatheusantunes.dev@gmail.com>
2023-06-06 16:23:17 -0300

    alter build

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.set('view engine', 'ejs');
@@ -10,4 +11,4 @@ app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.listen(process.env.APP_PORT, () => console.log('server running'));
+app.listen(process.env.APP_PORT, () => console.log('server running on port ' + process.env.APP_PORT));
